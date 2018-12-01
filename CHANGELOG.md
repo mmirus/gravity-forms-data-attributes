af25519ff05028b1a7d39238b93ecd8421f7211e
Matt Mirus <matt@mattmirus.com>
2018-12-01 17:49:23 -0500

    Add PHP linting

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## 1.0.3 / 2018-12-1
+
+- Add PHP linting
+
 ## 1.0.2 / 2018-12-1
 
 - Fix for multiselect dropdowns
--- gravity-forms-data-attributes.php
+++ gravity-forms-data-attributes.php
@@ -5,7 +5,7 @@ Plugin URI: https://github.com/mmirus/gravity-forms-data-attributes
 Description: Add custom data attributes to Gravity Forms fields.
 Author: Matt Mirus
 Author URI: https://github.com/mmirus
-Version: 1.0.2
+Version: 1.0.3
 GitHub Plugin URI: https://github.com/mmirus/gravity-forms-data-attributes
  */
 
