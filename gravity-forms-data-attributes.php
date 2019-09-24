90d5c658c9d18ac7c3be7f5581e17284fd054977
Zoltan Balogh <reddomester@gmail.com>
2019-09-24 14:48:17 +0300

    Reset repository links

--- gravity-forms-data-attributes.php
+++ gravity-forms-data-attributes.php
@@ -1,12 +1,12 @@
 <?php
 /*
 Plugin Name: Gravity Forms Data Attributes
-Plugin URI: https://github.com/reddo/gravity-forms-data-attributes
+Plugin URI: https://github.com/mmirus/gravity-forms-data-attributes
 Description: Add custom data attributes to Gravity Forms fields.
 Author: Matt Mirus
 Author URI: https://github.com/mmirus
 Version: 1.0.5
-GitHub Plugin URI: https://github.com/reddo/gravity-forms-data-attributes
+GitHub Plugin URI: https://github.com/mmirus/gravity-forms-data-attributes
  */
 
 namespace GFDA;
